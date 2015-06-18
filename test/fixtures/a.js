7861092f79860ea8cc6b06dc4423d6f2eb6fd2cc
Evan Cowden <evan38109@gmail.com>
2015-06-17 21:29:42 -0500

    implements index.js

--- test/fixtures/a.js
+++ test/fixtures/a.js
@@ -1,1 +1,3 @@
-module.exports = "a.js";
+module.exports = {
+  component: "a.js"
+};
--- test/indexSpec.js
+++ test/indexSpec.js
@@ -14,12 +14,35 @@ describe("index", function() {
     exclude: ['**/*Spec.js']
   }
 
-  it('returns a Highland stream', function(done) { // WRONG!!!
+  it('returns a promise that resolves to an object', function(done) { // WRONG!!!
+    var actual = scoop(options)
+      .then(function(modules) {
+        expect(modules).to.be.an('object');
+      })
+      .then(done)
+      .catch(done);
+  });
 
-    var actual = scoop(options);
+  it("maps files to their require()'d module", function(done) { // WRONG!!!
+    var actual = scoop(options)
+      .then(function(modules) {
+        expect(modules['a.js']).to.equal(require('./fixtures/a.js'));
+      })
+      .then(done)
+      .catch(done);
+  });
 
-    expect(_.isStream(actual)).to.be.true;
-    actual.done(done);
+  it('full integration test', function(done) {
+    var actual = scoop(options)
+      .then(function(modules) {
+        expect(modules['a.js']).to.equal(require('./fixtures/a.js'));
+        expect(modules['aSpec.js']).to.be.undefined;
+        expect(modules['b.json']).to.equal(require('./fixtures/b.json'));
+        expect(modules['c.junk']).to.be.undefined;
+        expect(modules['dir1/inner.js']).to.equal(require('./fixtures/dir1/inner.js'));
+      })
+      .then(done)
+      .catch(done);
   });
 
 
